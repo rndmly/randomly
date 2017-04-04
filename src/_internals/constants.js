7332f588e6a57a66605c01e2c05013a73b41d556
Richard Szakacs <richardszkcs@gmail.com>
2017-04-04 08:23:39 +0200

    Add constants for getRandomFloat()

--- src/_internals/constants.js
+++ src/_internals/constants.js
@@ -2,6 +2,7 @@
 
 
 module.exports = {
+
     /**
      * The default boolean bias.
      *
@@ -12,4 +13,53 @@ module.exports = {
      * @type {number}
      */
     _DEFAULT_BOOLEAN_BIAS : .5,
+
+
+    /**
+     * The default minimum float value.
+     *
+     * @private
+     * @internal
+     * @const
+     *
+     * @type {number}
+     */
+    _DEFAULT_FLOAT_MIN : 0,
+
+
+    /**
+     * The default maximum float value.
+     *
+     * @private
+     * @internal
+     * @const
+     *
+     * @type {number}
+     */
+    _DEFAULT_FLOAT_MAX : 1,
+
+
+    /**
+     * The default inclusion of the minimum value.
+     *
+     * @private
+     * @internal
+     * @const
+     *
+     * @type {boolean}
+     */
+    _DEFAULT_INCLUDE_MIN : true,
+
+
+    /**
+     * The default inclusion of the maximum value.
+     *
+     * @private
+     * @internal
+     * @const
+     *
+     * @type {boolean}
+     */
+    _DEFAULT_INCLUDE_MAX : false,
+
 };
