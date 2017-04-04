2b8ce34a2bdb03c5d3759e7fb7e223fa1e170f6a
Richard Szakacs <richardszkcs@gmail.com>
2017-04-04 07:54:43 +0200

    Add initial internal constants.js

---
+++ src/_internals/constants.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = {
+    /**
+     * The default boolean bias.
+     *
+     * @private
+     * @internal
+     * @const
+     *
+     * @type {number}
+     */
+    _DEFAULT_BOOLEAN_BIAS : .5,
+};
