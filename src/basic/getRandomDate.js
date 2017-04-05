bcaecef5f2f85791d26514833a6524449c12f356
Richard Szakacs <richardszkcs@gmail.com>
2017-04-05 07:08:07 +0200

    Restructure getRandomDate()

---
+++ src/basic/getRandomDate.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+/**
+ * Options type for the function getRandomDate().
+ *
+ * @typedef {Object|null} RandomDateOptions
+ */
+
+
+/**
+ * Returns a random element from an object.
+ *
+ * @param {RandomDateOptions} [options] - Set further options.
+ *
+ * @returns {Date}
+ */
+function getRandomDate(options = null)
+{
+    // TODO: implement function
+    return new Date();
+}
+
+
+module.exports = getRandomDate;
--- src/basic/index.js
+++ src/basic/index.js
@@ -1,39 +1,15 @@
 'use strict';
 
 
-const isPlainObject = require('../_internals/isPlainObject');
-
-
-/**
- * Options type for the function getRandomDate().
- *
- * @typedef {Object|null} RandomDateOptions
- */
-
-
-/**
- * Returns a random element from an object.
- *
- * @param {RandomDateOptions} [options] - Set further options.
- *
- * @returns {Date}
- */
-function getRandomDate(options = null)
-{
-    // TODO: implement function
-    return new Date();
-}
-
-
 module.exports = {
-    getRandomBool   : require('./getRandomBool'),
-    getRandomInt    : require('./getRandomInt'),
-    getRandom       : require('./getRandomFloat'),
-    getRandomFloat  : require('./getRandomFloat'),
-    getRandomString : require('./getRandomString'),
-    getRandomArray  : require('./getRandomArray'),
-    getRandomObject : require('./getRandomObject'),
-    getRandomDate,
+    getRandomBool    : require('./getRandomBool'),
+    getRandomInt     : require('./getRandomInt'),
+    getRandom        : require('./getRandomFloat'),
+    getRandomFloat   : require('./getRandomFloat'),
+    getRandomString  : require('./getRandomString'),
+    getRandomArray   : require('./getRandomArray'),
+    getRandomObject  : require('./getRandomObject'),
+    getRandomDate    : require('./getRandomDate')  ,
     getRandomPick    : require('./getRandomPick'),
     getRandomShuffle : require('./getRandomShuffle'),
 };
