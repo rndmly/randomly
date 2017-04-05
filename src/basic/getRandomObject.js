cc0c436f3976ce247bb9bea2a6faa213f22c71fb
Richard Szakacs <richardszkcs@gmail.com>
2017-04-05 06:59:43 +0200

    Restructure getRandomObject()

---
+++ src/basic/getRandomObject.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+/**
+ * Options type for the function getRandomObject().
+ *
+ * @typedef {Object|null} RandomObjectOptions
+ */
+
+
+/**
+ * Returns a random object.
+ *
+ * @param {RandomObjectOptions} options - Set further options.
+ *
+ * @returns {Object}
+ */
+function getRandomObject(options = null)
+{
+    // TODO: implement method
+    return {};
+}
+
+
+module.exports = getRandomObject;
--- src/basic/index.js
+++ src/basic/index.js
@@ -5,27 +5,6 @@ const isPlainObject = require('../_internals/isPlainObject');
 
 
 /**
- * Options type for the function getRandomObject().
- *
- * @typedef {Object|null} RandomObjectOptions
- */
-
-
-/**
- * Returns a random object.
- *
- * @param {RandomObjectOptions} options - Set further options.
- *
- * @returns {Object}
- */
-function getRandomObject(options = null)
-{
-    // TODO: implement method
-    return {};
-}
-
-
-/**
  * Options type for the function getRandomDate().
  *
  * @typedef {Object|null} RandomDateOptions
@@ -53,7 +32,7 @@ module.exports = {
     getRandomFloat  : require('./getRandomFloat'),
     getRandomString : require('./getRandomString'),
     getRandomArray  : require('./getRandomArray'),
-    getRandomObject,
+    getRandomObject : require('./getRandomObject'),
     getRandomDate,
     getRandomPick    : require('./getRandomPick'),
     getRandomShuffle : require('./getRandomShuffle'),
